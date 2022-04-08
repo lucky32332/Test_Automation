ac1156e8078f4a764f99de2a0aa0e0c39d707fb1
lucky32332 <lucky00@abv.bg>
2022-04-08 21:48:28 +0300

    update

--- src/test/java/test/ProductTests.java
+++ src/test/java/test/ProductTests.java
@@ -17,19 +17,12 @@ public class ProductTests extends TestUtil {
         ProductsPage productsPage = loginPage.login("standard_user", "secret_sauce");
         productsPage.addItemToTheCart("onesie");
 
+
+
         //Hard Assert
         Assert.assertEquals(productsPage.getItemsInTheCart(), 1, "Because we have only one item so far");
 
-        productsPage.removeItemFromTheCart("onesie");
 
-        //Soft Assert
-        SoftAssert softAssert = new SoftAssert();
-
-        softAssert.assertEquals(productsPage.getItemsInTheCart(), 0, "Because we have only one item so far");
-        System.out.println("I will be executed");
-
-        //for example at the end of the test
-        softAssert.assertAll(); // now all asserts
 
 
     }
