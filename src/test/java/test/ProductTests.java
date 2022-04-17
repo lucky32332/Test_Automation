042fb77058286049c1380e78ce603874469ce0a6
lucky32332 <lucky00@abv.bg>
2022-04-17 22:05:47 +0300

    update

--- src/test/java/test/ProductTests.java
+++ src/test/java/test/ProductTests.java
@@ -3,6 +3,8 @@ package test;
 import Pages.LoginPage;
 import Pages.ProductsPage;
 import base.TestUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -16,12 +18,16 @@ public class ProductTests extends TestUtil {
         LoginPage loginPage = new LoginPage(driver);
         ProductsPage productsPage = loginPage.login("standard_user", "secret_sauce");
         productsPage.addItemToTheCart("onesie");
+        productsPage.nextStep();
+
 
 
 
         //Hard Assert
         Assert.assertEquals(productsPage.getItemsInTheCart(), 1, "Because we have only one item so far");
 
+        WebElement itemView = driver.findElement(By.xpath("//*[contains(@class,'inventory_item_name')][1]"));
+        Assert.assertTrue(itemView.isDisplayed(), "This shall be visible after add product");
 
 
 
